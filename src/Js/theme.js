e4bd0b08589fef23dd90f4f3f52f4f9d9bf0fd6c
Mohsen <mohsen63296329@gmail.com>
2024-03-14 15:05:38 +0330

    Fix icon in dark and light mode

--- src/Js/theme.js
+++ src/Js/theme.js
@@ -2,6 +2,9 @@ const $ = document
 
 const html = $.querySelector('.html')
 const themeIcon = $.querySelector('#theme')
+const cartIcon = $.querySelector('#cartIcon')
+const caretIcon = $.querySelector('#caretIcon')
+
 let isDarkTheme = false
 
 // Function to toggle the theme
@@ -17,6 +20,25 @@ function changeTheme() {
 	isDarkTheme = JSON.parse(localStorage.getItem('theme'))
 	// Check the actual value and update classList
 	isDarkTheme ? html.classList.add('dark') : html.classList.remove('dark')
+	
+	// Change Icon Color
+	isDarkTheme
+		? themeIcon.setAttribute('src', './public/icons/Soild/moon.svg')
+		: themeIcon.setAttribute('src', './public/icons/Soild/sun.svg')
+	
+		isDarkTheme
+		? cartIcon.setAttribute('src', './public/icons/Soild/shopping-cart-light.svg')
+		: cartIcon.setAttribute('src', './public/icons/Soild/shopping-cart-dark.svg')
+		
+		isDarkTheme
+		? caretIcon.setAttribute('src', './public/icons/Soild/Caret-left-light.svg')
+		: caretIcon.setAttribute('src', './public/icons/Soild/Caret-left.svg')
+}
+
+function changeAngleIcon () {
+	const browserWidth = window.innerWidth
+
+	browserWidth <= 500 ? caretIcon.style.transform = 'rotate(90deg)' : caretIcon.style.transform = 'rotate(0deg)' 
 }
 
 // Event listener for theme toggle
@@ -24,3 +46,7 @@ themeIcon.addEventListener('click', toggleTheme)
 
 // Event listener for window load to initially set the theme
 window.addEventListener('load', changeTheme)
+
+// Event listener for browser size to change caret angle
+window.addEventListener('resize', changeAngleIcon)
+changeAngleIcon()
