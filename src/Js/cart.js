20710f5f314097ea54f08fde69ee08e0f396291d
Mohsen <mohsen63296329@gmail.com>
2024-04-12 19:36:50 +0330

    Create changeProductCount function in cart

--- src/Js/cart.js
+++ src/Js/cart.js
@@ -3,7 +3,6 @@
 const cartProducts = JSON.parse(localStorage.getItem('products'))
 
 const productsContainer = $.querySelector('#products-container')
-const cartCount = $.querySelector('#cartCount')
 const emptyCartElem = $.querySelector('.emptyCart')
 
 // Show Products in Cart
@@ -59,16 +58,16 @@ function showProducts(datas) {
                 <p class="dark:text-grayText">Count</p>
 
                 <div class="bg-gray h-6 flex items-center gap-2 rounded-full">
-                  <span
-                    class="h-full flex items-center justify-center w-4 rounded-l-full bg-white text-black select-none text-xl cursor-pointer"
+                  <span id="product-${data.id}-minus"
+                    class="countChanger h-full flex items-center justify-center w-4 rounded-l-full bg-white text-black select-none text-xl cursor-pointer"
                   >
                     -
                   </span>
 
-                  <span>1</span>
+                  <span id="product-${data.id}-count">1</span>
 
-                  <span
-                    class="h-full flex items-center justify-center w-4 rounded-r-full bg-white text-black select-none text-lg cursor-pointer"
+                  <span id="product-${data.id}-plus"
+                    class="countChanger h-full flex items-center justify-center w-4 rounded-r-full bg-white text-black select-none text-lg cursor-pointer"
                   >
                     +
                   </span>
@@ -86,7 +85,7 @@ function showProducts(datas) {
 		)
 	})
 
-  // Check if cart is empty
+	// Check if cart is empty
 	emptyCart()
 }
 
@@ -97,8 +96,8 @@ function errorFetchDatas() {
 
 // Delete Product from cart
 function deleteProduct(productIdElem) {
-  // Get Products
-  const cartProducts = JSON.parse(localStorage.getItem('products'))
+	// Get Products
+	const cartProducts = JSON.parse(localStorage.getItem('products'))
 
 	// Filter out the product to delete
 	const filteredProducts = cartProducts.filter(
@@ -112,23 +111,96 @@ function deleteProduct(productIdElem) {
 	productsContainer.innerHTML = ''
 	showProducts(filteredProducts)
 
-  // Check if cart is empty
+	// Check if cart is empty
 	emptyCart()
 }
 
 // Show Empty Cart Picture
 function emptyCart() {
-  // Get Products
-  const cartProducts = JSON.parse(localStorage.getItem('products'))
+	// Get Products
+	const cartProducts = JSON.parse(localStorage.getItem('products'))
 
-  if (cartProducts.length == 0) {
-    emptyCartElem.classList.remove('hidden')
-  } else {
-    !emptyCartElem.classList.value.includes('hidden') &&
-      emptyCartElem.classList.add('hidden')
-  }
+	if (cartProducts.length == 0) {
+		emptyCartElem.classList.remove('hidden')
+	} else {
+		!emptyCartElem.classList.value.includes('hidden') &&
+			emptyCartElem.classList.add('hidden')
+	}
+}
+
+// Change Product Count
+function changeProductCount(countChanger) {
+	// Get product id number
+	const idNumber = countChanger.id.match(/\d+/)[0]
+
+	// Get all products count from local storage
+	const productsCount = JSON.parse(localStorage.getItem('products-count'))
+
+	// Get product count
+	const filteredProductCount = productsCount.find(
+		(product) => product.id == idNumber
+	)
+
+	const countElem = document.querySelector(`#product-${idNumber}-count`)
+
+	let countNumber = filteredProductCount.count
+
+	// Increasing product count
+	if (countChanger.id.includes('plus')) {
+		countNumber++
+	}
+
+	// Decreasing product count
+	if (countChanger.id.includes('minus')) {
+		countNumber--
+
+		// If the count reaches zero, remove product from local storage
+		if (countNumber == 0) {
+			const products = JSON.parse(localStorage.getItem('products'))
+
+			const indexToRemove = productsCount.findIndex((e) => e.id == idNumber)
+
+			// Remove the product from the productsCount array if found
+			if (indexToRemove !== -1) {
+				productsCount.splice(indexToRemove, 1)
+			}
+
+			// Also, remove the corresponding product from the products array
+			const productIndexToRemove = products.findIndex(
+				(product) => product.id == idNumber
+			)
+
+			if (productIndexToRemove !== -1) {
+				products.splice(productIndexToRemove, 1)
+			}
+
+			// Update localStorage with the modified arrays
+			localStorage.setItem('products-count', JSON.stringify(productsCount))
+			localStorage.setItem('products', JSON.stringify(products))
+
+			// Update Show Products
+			productsContainer.innerHTML = ''
+			showProducts(products)
+		}
+	}
+
+	filteredProductCount.count = countNumber
+	countElem.innerHTML = countNumber
+
+	// Update the products count in local storage
+	localStorage.setItem('products-count', JSON.stringify(productsCount))
 }
 
 window.addEventListener('load', () => {
 	showProducts(cartProducts)
+
+	// Get count changer buttons
+	const countChangers = $.querySelectorAll('.countChanger')
+
+	// Get the button to change the count when clicked
+	countChangers.forEach((countChanger) => {
+		countChanger.addEventListener('click', () =>
+			changeProductCount(countChanger)
+		)
+	})
 })
