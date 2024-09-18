adf48ce3c34e236e70047132022b72c615152edf
Mohsen <mohsen63296329@gmail.com>
2024-09-18 20:40:44 +0330

    Create open nav and close nav for mobile

--- src/Js/navigation.js
+++ src/Js/navigation.js
@@ -4,6 +4,7 @@ const navBtn = $.querySelector('#nav')
 const navContainer = $.querySelector('#navigation-container')
 const navigation = $.querySelector('#navigation')
 const navCaretIcon = $.querySelector('#caretIcon')
+const wishlistIcon = $.querySelector('#wishlist-icon')
 
 let isNavOpen = false
 
@@ -17,8 +18,17 @@ const toggleNavigation = () => {
 			closeNavigationDesktop()
 		}
 	}
+
+	if (screenWidth <= 640) {
+		if (!isNavOpen) {
+			openNavigationMobile()
+		} else {
+			closeNavigationMobile()
+		}
+	}
 }
 
+// Open navigation for desktop
 const openNavigationDesktop = () => {
 	navigation.style.width = '100%'
 	navigation.style.padding = '2.5rem 10px 0 0'
@@ -32,11 +42,37 @@ const openNavigationDesktop = () => {
 	isNavOpen = true
 }
 
+// Close navigation for desktop
 const closeNavigationDesktop = () => {
 	navigation.style.cssText = ''
 	navContainer.style.cssText = ''
 
 	navCaretIcon.style.transform = 'rotate(0deg)'
+	isNavOpen = false
+}
+
+// Open navigation for mobile
+const openNavigationMobile = () => {
+	navigation.style.alignItems = 'end'
+
+	navContainer.style.height = '10rem'
+	navContainer.style.bottom = '0'
+	navContainer.style.alignItems = 'end'
+
+	navCaretIcon.style.transform = 'rotate(270deg)'
+	wishlistIcon.style.marginBottom = '2rem'
+
+	isNavOpen = true
+}
+
+// Close navigation for mobile
+const closeNavigationMobile = () => {
+	navigation.style.cssText = ''
+	navContainer.style.cssText = ''
+
+	wishlistIcon.style.marginBottom = '0'
+
+	navCaretIcon.style.transform = 'rotate(90deg)'
 	isNavOpen = false
 }
 
