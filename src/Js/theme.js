d8507124a95544bb14ee5897819a71f4a52ae15d
Mohsen <mohsen63296329@gmail.com>
2024-12-19 22:19:49 +0330

    Clean some codes

--- src/Js/theme.js
+++ src/Js/theme.js
@@ -17,39 +17,50 @@ function toggleTheme() {
 
 // Function to apply the theme based on the local storage value
 function changeTheme() {
-	const isHomePage = location.pathname.includes('ShopHub') || location.pathname == '/'
+	const isHomePage =
+		location.pathname.includes('ShopHub') ||
+		location.pathname.includes('index') ||
+		location.pathname === '/'
 
 	// Use JSON.parse to convert the stored value to a boolean
 	isDarkTheme = JSON.parse(localStorage.getItem('isDarkTheme'))
 	// Check the actual value and update classList
 	isDarkTheme ? html.classList.add('dark') : html.classList.remove('dark')
-	
+
 	// Change Icon Color
 	isDarkTheme
 		? themeIcon.setAttribute('src', './public/icons/Soild/moon.svg')
 		: themeIcon.setAttribute('src', './public/icons/Soild/sun.svg')
 
-		if(isHomePage) {
-			isDarkTheme
-			? cartIcon.setAttribute('src', './public/icons/Soild/shopping-cart-light.svg')
-			: cartIcon.setAttribute('src', './public/icons/Soild/shopping-cart-dark.svg')
-		}
+	if (isHomePage) {
+		isDarkTheme
+			? cartIcon.setAttribute(
+					'src',
+					'./public/icons/Soild/shopping-cart-light.svg'
+			  )
+			: cartIcon.setAttribute(
+					'src',
+					'./public/icons/Soild/shopping-cart-dark.svg'
+			  )
+	}
 
-		if(!isHomePage) {
-			isDarkTheme
+	if (!isHomePage) {
+		isDarkTheme
 			? homeIcon.setAttribute('src', './public/icons/Soild/home-light.svg')
 			: homeIcon.setAttribute('src', './public/icons/Soild/home-dark.svg')
-		}
-		
-		isDarkTheme
+	}
+
+	isDarkTheme
 		? caretIcon.setAttribute('src', './public/icons/Soild/Caret-left-light.svg')
 		: caretIcon.setAttribute('src', './public/icons/Soild/Caret-left.svg')
 }
 
-function changeAngleIcon () {
+function changeAngleIcon() {
 	const browserWidth = window.innerWidth
 
-	browserWidth <= 500 ? caretIcon.style.transform = 'rotate(90deg)' : caretIcon.style.transform = 'rotate(0deg)' 
+	browserWidth <= 500
+		? (caretIcon.style.transform = 'rotate(90deg)')
+		: (caretIcon.style.transform = 'rotate(0deg)')
 }
 
 // Event listener for theme toggle
